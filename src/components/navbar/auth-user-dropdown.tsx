bd342556cc568c79ab04864e2431c9ab4c3f2f8f
batuhanbaybas <b.baybas@gmail.com>
2026-06-12 04:14:12 +0300

    refactor: update AuthUserDropdown to use Avatar and session data
    - Replaced UserIcon and Button with Avatar component for user display.
    - Integrated useSession hook to fetch user session data.
    - Updated sign-out functionality to use onSelect instead of onClick.

--- src/components/navbar/auth-user-dropdown.tsx
+++ src/components/navbar/auth-user-dropdown.tsx
@@ -1,28 +1,32 @@
-import { UserIcon } from "lucide-react"
-import { Button } from "../ui/button"
 import Dropdown from "../ui/dropdown"
-import { signOut } from "better-auth/api"
+import { signOut, useSession } from "#/lib/auth-client"
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar"
 
 function AuthUserDropdown() {
+  const { data: session } = useSession()
 
-
-const handleSignOut = async () => {
-  await signOut()
-}
-
+  const handleSignOut = async () => {
+    await signOut()
+  }
 
   return (
     <Dropdown
-      triggerProps={{
-        children: <Button variant="ghost" size="icon">
-          <UserIcon className="w-4 h-4" />
-        </Button>
-      }}
+      triggerProps={
+        {children: <Avatar className="size-8">
+          <AvatarImage src={session?.user.image ?? ""} />
+          <AvatarFallback>
+            {session?.user.name
+              .split(" ")
+              .map((name) => name[0])
+              .join("")}
+          </AvatarFallback>
+        </Avatar>
+        }}
       items={[
         {
           label: "Logout",
-          onClick: handleSignOut,
-        }
+          onSelect: handleSignOut,
+        },
       ]}
     />
   )
