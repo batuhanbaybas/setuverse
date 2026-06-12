bf879e0dc6e6bcfb313eef0c440ceb103a91d8d8
batuhanbaybas <b.baybas@gmail.com>
2026-06-12 04:59:39 +0300

    refactor: link buttun created
    link button created based on button variant rules

---
+++ src/components/ui/button/link-button.tsx
@@ -0,0 +1,22 @@
+import { Link } from "@tanstack/react-router"
+import { buttonVariants } from "./button-variant"
+import type { VariantProps } from "class-variance-authority"
+import { cn } from "#/lib/utils"
+
+
+function LinkButton({
+  variant = "default",
+  size = "default",
+  ...props
+}: React.ComponentProps<typeof Link> &
+  VariantProps<typeof buttonVariants>) {
+
+  return (
+    <Link
+      className={cn(buttonVariants({ variant, size }))}
+      {...props}
+    />
+  )
+}
+
+export default LinkButton
