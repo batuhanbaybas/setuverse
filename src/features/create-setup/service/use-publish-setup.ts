a57546a8e675b7912c3dc956ba0abe0a26ff538d
batuhanbaybas <b.baybas@gmail.com>
2026-06-19 04:00:06 +0300

    refactor: update usePublishSetup to utilize useServerFn for improved mutation handling
    - Replaced direct function call with useServerFn in usePublishSetup for better integration with server functions.
    - Adjusted mutation function to pass input data correctly, enhancing data handling consistency.

--- src/features/create-setup/service/use-publish-setup.ts
+++ src/features/create-setup/service/use-publish-setup.ts
@@ -4,11 +4,13 @@ import { queryKeys } from '#/features/create-setup/lib/query-keys'
 
 import { publishSetupFn } from '../server/publish-setup.functions'
 import type { PublishSetupInput } from '../server/publish-setup.functions'
+import { useServerFn } from '@tanstack/react-start'
 
 const usePublishSetup = () => {
+  const publishSetup = useServerFn(publishSetupFn)
   return useMutation({
     mutationKey: [queryKeys.publishSetup],
-    mutationFn: (input: PublishSetupInput) => publishSetupFn({ data: input }),
+    mutationFn: (input: PublishSetupInput) => publishSetup({ data: input }),
   })
 }
 
